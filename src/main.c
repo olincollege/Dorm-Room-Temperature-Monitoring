274a64c292d972ea8d3cf1b3119ddcc3dafb9b04
Vishnu Eskew <vishnu.eskew@gmail.com>
2025-05-04 16:24:33 -0400

    Modify main to fix a pointer issue with storing temp and humidity, and add database code to src/main.

--- src/main.c
+++ src/main.c
@@ -2,16 +2,17 @@
 #include <stdlib.h>
 
 #include "sensor.h"
+#include "database_insert.h"
+#include "database_create.h"
 
 int main() {
-    //create database
+    create_database("sensor.db");
     //create server
-    float* temp;
-    float* humidity;
+    float temp;
+    float humidity;
     // loop (infinitely, or until command interrupt?)
-        get_measurement(SHT40_ADDR, temp, humidity);
-        // insert temp and humidity readings into database
-
+        get_measurement(SHT40_ADDR, &temp, &humidity);
+        insert_sensor_reading("sensor.db", temp, humidity, "Sensor 01");
 
     printf("Hello World!");
     print_measurements(SHT40_ADDR, 10, 1);
